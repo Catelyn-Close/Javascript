0e458a03f6a541a5397107791e1ce9d7dd80c064
Catelyn-Close <catie.did75@gmail.com>
2025-04-17 14:33:29 -0700

    Create Javascript.js

---
+++ Javascript.js
@@ -0,0 +1,20 @@
+//Javascript Code
+
+//Log and console reassignment
+
+let character = 'Hello';
+console.log(character);
+character = "World";
+let secondCharacter;
+secondCharacter = character;
+console.log(secondCharacter);
+
+//arrays
+
+let count = 8;
+let rows = ["Naomi", "Quincy", "CamperChan"];
+console.log(rows[0]);
+rows[2] = 10;
+console.log(rows)
+rows[rows.length - 1] = 10;
+
